df92373163b65a77e7d3a527137b46a014d46d78
Ryan Alves <alvsryan02@gmail.com>
2026-05-24 11:47:59 -0300

    Update even or odd numbers.py

--- even or odd numbers.py
+++ even or odd numbers.py
@@ -4,4 +4,4 @@ result = int(((num1+num2)**4)/2)
 if result % 2 == 0:
     print(f'The number {result} is even!')
 else:
-    print(The number {result} is odd!')
+    print(f'The number {result} is odd!')
