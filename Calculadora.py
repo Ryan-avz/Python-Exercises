16bb4119aa6d911989579fd0837d2ed188aafb61
Ryan Alves <alvsryan02@gmail.com>
2026-06-11 21:20:43 -0300

    Update-calculator.py
    It's automatic now!!

--- Calculadora.py
+++ Calculadora.py
@@ -1,36 +1,46 @@
-# only +, -, * and / working 
-quest = input('Choose an arithmetic operator to use: ')
 
+i = 0
 # THE AZTEC CALCULATOR
+while i < 1:
 
-if quest == "*":
+ quest = input('| addition (+) \n'
+               '| subtraction (-) \n'
+               '| multiplication (*) \n'
+               '| division (/) \n'
+               'Choose an arithmetic operator to use: ')
+
+ if quest == "*":
     num1 = int(input('Enter a number: '))
     num2 = int(input('Enter another number: '))
     calc3 = (num1 * num2)
     print(calc3)
+    i = int(input('0 : continue \n'
+                  '1 : stop \n '))
 
-elif quest == "/":
+ elif quest == "/":
     num1 = int(input('Enter a number: '))
     num2 = int(input('Enter another number: '))
     calc4 = (num1 / num2)
     print(calc4)
+    i = int(input('0 : continue \n'
+                  '1 : stop \n '))
 
-elif quest == "-":
+ elif quest == "-":
     num1 = int(input('Enter a number: '))
     num2 = int(input('Enter another number: '))
     calc5 = (num1 - num2)
     print(calc5)
+    i = int(input('0 : continue \n'
+                  '1 : stop \n '))
 
-elif quest == "+":
+ elif quest == "+":
     num1 = int(input('Enter a number: '))
     num2 = int(input('Enter another number: '))
     calc6 = (num1 + num2)
     print(calc6)
+    i = int(input('0 : continue \n'
+                  '1 : stop \n '))
 
 else:
-    arg = input('Are you really going to do this to me? ')
-
-    while arg != 'no':
-        print('nooooooooooooooooooooooooooooooooooooo')
-        break
+    print('Stopped!')
 
