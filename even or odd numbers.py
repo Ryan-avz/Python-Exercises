d95293768d430c4c1c82acaa8c9e009e9c5cb61b
Ryan Alves <alvsryan02@gmail.com>
2026-08-20 11:24:21 -0300

    Update even or odd numbers.py
    Update: Refactored the even/odd checker by creating a dedicated function, simplifying the boolean logic, and adding a loop to allow repeated number checks until the user chooses to stop.

--- even or odd numbers.py
+++ even or odd numbers.py
@@ -1,7 +1,18 @@
-num1 = int(input('Enter any number: '))
-num2 = int(input('Enter another number: '))
-result = int(((num1+num2)**4)/2)
-if result % 2 == 0:
-    print(f'The number {result} is even!')
-else:
-    print(f'The number {result} is odd!')
+def evenOdd_check(x: int):
+    if x % 2 == 0:
+        return True
+    if x % 2 == 1:
+        return False
+choice = 0
+
+while choice != 1:
+    num = int(input('Number: '))
+
+    if evenOdd_check(num):
+        print("Even!")
+    else:
+        print("Odd!")
+
+    choice = int(input('1 to stop, 0 to continue: '))
+
+print("Stopped!")
